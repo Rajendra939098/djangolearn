714c2e03786e13c56003beb303a555ca73629f85
Rajendra Atla <atlarajendra33@gmail.com>
2023-12-12 21:59:01 +0530

    E-commerce

--- test_project/test_app/urls.py
+++ test_project/test_app/urls.py
@@ -4,6 +4,8 @@ from . import views
 urlpatterns=[
     path('home/',views.home,name='home'),
     path('blog/',views.blog,name="blog"),
-    path('login/',views.login,name='login'),
-    path('register/',views.register,name='register')
+    path('',views.login,name='login'),
+    path('register/',views.register,name='register'),
+    path('product/',views.product,name='product'),
+    path('order/',views.order,name='order')
 ]
--- test_project/test_app/views.py
+++ test_project/test_app/views.py
@@ -1,13 +1,48 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import *
 # Create your views here.
 def home(request):
+    
     return render(request,'test_app/home.html')
 
 def blog(request):
-    return render(request,"test_app/blog.html")
+    orders=Order.objects.all()
+    customers=customer.objects.all()
+    
+    total_customers=customers.count()
+    total_orders=orders.count()
+    delivered=orders.filter(status='Delivered').count()
+    pending=orders.filter(status='pending').count()
+
+
+    context={'orders':orders,'customers':customers,'total_orders':total_orders,
+             'delivered':delivered,'pending':pending,'total_customers':total_customers }
+
+
+    return render(request,"test_app/blog.html",context)
 
 def login(request):
     return render(request,'test_app/login.html')
 def register(request):
     return render(request,'test_app/register.html')
+def product(request):
+    products = Product.objects.all()
+
+    return render(request,'test_app/product.html',{'products':products})
+def order(request):
+
+    orders=Order.objects.all()
+    customers=customer.objects.all()
+    
+    total_customers=customers.count()
+    total_orders=orders.count()
+    delivered=orders.filter(status='Delivered').count()
+    pending=orders.filter(status='pending').count()
+
+
+    context={'orders':orders,'customers':customers,'total_orders':total_orders,
+             'delivered':delivered,'pending':pending,'total_customers':total_customers }
+
+
+    return render(request,'test_app/order.html',context)
